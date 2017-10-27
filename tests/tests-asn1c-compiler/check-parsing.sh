1203cf96d0a0f20cb0a5d8828f6203d6043944c9
Vasil Velichkov <vvvelichkov@gmail.com>
2017-10-28 00:39:17 +0300

    Set LC_ALL to C n check-parsing.sh
    This fixes the following error on MacOS
    Checking ../../tests/tests-asn1c-compiler/16-constraint-OK.asn1 against ../../tests/tests-asn1c-compiler/16-constraint-OK.asn1.-EFprint-constraints
    sed: RE error: illegal byte sequence
    
    The problem is that some of the test files contain non-UTF8 characters

--- tests/tests-asn1c-compiler/check-parsing.sh
+++ tests/tests-asn1c-compiler/check-parsing.sh
@@ -31,9 +31,9 @@ for ref in ${top_srcdir}/tests/tests-asn1c-compiler/*.asn1.-*; do
 	template=.tmp.check-parsing.$$
 	oldversion=${template}.old
 	newversion=${template}.new
-	LANG=C sed -e 's/^found in .*/found in .../' < "$ref" > "$oldversion"
-	ec=0
-	(${top_builddir}/asn1c/asn1c -S ${top_srcdir}/skeletons -no-gen-OER -no-gen-PER "-$flags" "$src" | LANG=C sed -e 's/^found in .*/found in .../' > "$newversion") || ec=$?
+	PROCESSING="$ref (from $src)"
+	LC_ALL=C sed -e 's/^found in .*/found in .../' < "$ref" > "$oldversion"
+	(${top_builddir}/asn1c/asn1c -S ${top_srcdir}/skeletons "-$flags" "$src" | LC_ALL=C sed -e 's/^found in .*/found in .../' > "$newversion") || ec=$?
 	if [ $? = 0 ]; then
 		diff $diffArgs "$oldversion" "$newversion" || ec=$?
 	fi
