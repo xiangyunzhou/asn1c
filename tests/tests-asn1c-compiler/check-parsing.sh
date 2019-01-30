f49121bf830205cfaecd16504ec65d0b4b2f15b2
Mouse <mouse008@gmail.com>
2019-01-30 11:41:29 -0500

    Make sure "ec" is set before use

--- tests/tests-asn1c-compiler/check-parsing.sh
+++ tests/tests-asn1c-compiler/check-parsing.sh
@@ -33,6 +33,7 @@ for ref in ${top_srcdir}/tests/tests-asn1c-compiler/*.asn1.-*; do
 	newversion=${template}.new
 	PROCESSING="$ref (from $src)"
 	LC_ALL=C sed -e 's/^found in .*/found in .../' < "$ref" > "$oldversion"
+	ec=0
 	(${top_builddir}/asn1c/asn1c -S ${top_srcdir}/skeletons "-$flags" "$src" | LC_ALL=C sed -e 's/^found in .*/found in .../' > "$newversion") || ec=$?
 	if [ $? = 0 ]; then
 		diff $diffArgs "$oldversion" "$newversion" || ec=$?
