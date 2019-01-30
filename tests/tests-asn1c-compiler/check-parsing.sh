75b7467a842f069edc3d323f52e7e44902a4b2b0
Mouse <mouse008@gmail.com>
2019-01-30 11:50:57 -0500

    Restoring what worked before, plus adding fixes from @velichkov

--- tests/tests-asn1c-compiler/check-parsing.sh
+++ tests/tests-asn1c-compiler/check-parsing.sh
@@ -32,9 +32,8 @@ for ref in ${top_srcdir}/tests/tests-asn1c-compiler/*.asn1.-*; do
 	oldversion=${template}.old
 	newversion=${template}.new
 	PROCESSING="$ref (from $src)"
-	LC_ALL=C sed -e 's/^found in .*/found in .../' < "$ref" > "$oldversion"
-	ec=0
-	(${top_builddir}/asn1c/asn1c -S ${top_srcdir}/skeletons "-$flags" "$src" | LC_ALL=C sed -e 's/^found in .*/found in .../' > "$newversion") || ec=$?
+	LANG=C LC_ALL=C sed -e 's/^found in .*/found in .../' < "$ref" > "$oldversion"
+	(${top_builddir}/asn1c/asn1c -S ${top_srcdir}/skeletons -no-gen-OER -no-gen-PER "-$flags" "$src" | LANG=C LC_ALL=C sed -e 's/^found in .*/found in .../' > "$newversion") || ec=$?
 	if [ $? = 0 ]; then
 		diff $diffArgs "$oldversion" "$newversion" || ec=$?
 	fi
