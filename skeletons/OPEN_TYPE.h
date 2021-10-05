42a7525ff1579f1104f0064e7ec825f9cddd00ef
Senthil Prabakaran <senthilp@consumermesh.com>
2021-10-05 09:54:59 -0400

    Remove decode

--- skeletons/OPEN_TYPE.h
+++ skeletons/OPEN_TYPE.h
@@ -59,7 +59,6 @@ asn_dec_rval_t OPEN_TYPE_xer_get(
 #endif  /* !defined(ASN_DISABLE_XER_SUPPORT) */
 
 #if !defined(ASN_DISABLE_JER_SUPPORT)
-#define OPEN_TYPE_decode_jer NULL
 #define OPEN_TYPE_encode_jer CHOICE_encode_jer
 #endif  /* !defined(ASN_DISABLE_JER_SUPPORT) */
 
